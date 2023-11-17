8b6e5158f377a3d936cd985e0f89e882fde1ea7d
Darve99 <104438377+Darve99@users.noreply.github.com>
2023-11-17 18:16:08 -0500

    lista dsiponibles funcional

--- app/src/main/java/com/example/taller3/HomeActivity.kt
+++ app/src/main/java/com/example/taller3/HomeActivity.kt
@@ -9,6 +9,8 @@ import com.google.firebase.database.*
  class HomeActivity : AppCompatActivity() {
 
      private val usersList = mutableListOf<String>()
+     private lateinit var databaseReference: DatabaseReference
+     private lateinit var currentUserID: String
 
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
@@ -16,13 +18,40 @@ import com.google.firebase.database.*
 
          val lista = findViewById<ListView>(R.id.lista)
 
-
-
          val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, usersList)
          lista.adapter = adapter
+
+         // Referencia a la base de datos de Firebase
+         databaseReference = FirebaseDatabase.getInstance().reference.child("users")
+
+         // Obtener el ID del usuario actual
+         val currentUser = FirebaseAuth.getInstance().currentUser
+         currentUserID = currentUser?.uid ?: ""
+
+         loadUsers(adapter)
      }
 
      private fun loadUsers(adapter: ArrayAdapter<String>) {
+         val query = databaseReference.orderByChild("Estado").equalTo("Disponible")
+         query.addValueEventListener(object : ValueEventListener {
+             override fun onDataChange(snapshot: DataSnapshot) {
+                 usersList.clear()
+                 for (data in snapshot.children) {
+                     val userId = data.key // Obtener el ID del usuario de la base de datos
+                     // Omitir al usuario actual en la lista
+                     if (userId != currentUserID) {
+                         val userName = data.child("nombre").getValue(String::class.java)
+                         userName?.let {
+                             usersList.add(it)
+                         }
+                     }
+                 }
+                 adapter.notifyDataSetChanged()
+             }
 
+             override fun onCancelled(error: DatabaseError) {
+                 // Manejar errores al obtener datos
+             }
+         })
      }
  }
